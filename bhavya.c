3a0c7f522f414193f1bb2b0317d135750db9f377
Chaitra-Bhavya <50397667+Chaitra-Bhavya@users.noreply.github.com>
2019-05-08 18:03:37 +0530

    Update bhavya.c

--- bhavya.c
+++ bhavya.c
@@ -1,26 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n,rem,originalint=0;
-    int var=0;
-    n=12321;
+    int i, j, rows=5;
 
-    originalint=n;
+    for(i=rows; i>=1; --i)
+    {
+        for(j=1; j<=i; ++j)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
 
-    while(n!=0)
-    {
-        rem=n%10;
-        n=n/10;
-        var=var*10;
-        var=var+rem;
-    }
-    if(originalint==var)
-    {
-        printf(" palindrome");
-    }
-    else
-    {
-        printf("not a palindrome");
-    }
     return 0;
 }
