a7563399565a7ba9fdab08e39e2b839fb209f71d
Chaitra-Bhavya <50397667+Chaitra-Bhavya@users.noreply.github.com>
2019-05-08 17:51:35 +0530

    Update arun1.c

--- arun1.c
+++ arun1.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    double n1=5, n2=10, n3=15;
+    int n1=5, n2=10, n3=15;
 
     if( n1>=n2 && n1>=n3 )
-        printf("%.2f is the largest number.", n1);
+        printf("%d is the largest number.", n1);
 
     if( n2>=n1 && n2>=n3 )
-        printf("%.2f is the largest number.", n2);
+        printf("%d is the largest number.", n2);
 
     if( n3>=n1 && n3>=n2 )
-        printf("%.2f is the largest number.", n3);
+        printf("%d is the largest number.", n3);
 
     return 0;
 }
