86cec994a17b93e97bf3e0400d48807bfdf2f370
Chaitra-Bhavya <50397667+Chaitra-Bhavya@users.noreply.github.com>
2019-05-08 17:30:29 +0530

    Update arun1.c

--- arun1.c
+++ arun1.c
@@ -6,22 +6,6 @@ int main() {
  
    password="Abhagit";
    /* 13 is ASCII value of Enter key */
-   while((c = getch()) != 13){
-       if(index < 0)
-           index = 0;
-       /* 8 is ASCII value of BACKSPACE character */
-       if(c == 8){
-           putch('\b');
-           putch(NULL);
-           putch('\b');
-           index--;
-       continue;
-       }
-       password[index++] = c;
-       putch('*');
-   }
-   password[index] = '\0';
-
    printf("\nPassword String = %s", password);
  
    return 0;
